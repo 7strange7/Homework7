66632018c88328f9548b2a291756fdf8e8275541
Bradley Strange <bradleygstrange@gmail.com>
2025-10-02 16:00:32 -0600

    First challenege done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+// Debug.Assert(FrequencyType(3) == "Fire");
+// Debug.Assert(FrequencyType(20) == "Electric");
+
+Debug.Assert(Function1(target, guess) == )
+
+Console.Clear();
+// for (int crankNum = 1; crankNum <= 100; crankNum++)
+// {
+
+//     Console.Write($"{crankNum}: ");
+//     Console.WriteLine(FrequencyType(crankNum));
+
+// }
+
+// string FrequencyType(double element)
+// {
+//     if (element % 3 == 0) return "Fire";
+
+//     else if (element % 5 == 0) return "Electric";
+
+//     return "Normal";
+// }
